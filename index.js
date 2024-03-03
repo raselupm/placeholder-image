514078888cc60faeb530696035b8644dbe74e235
raselupm <wp@perfectpointmarketing.com>
2024-03-03 15:27:24 +0300

    font fix

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-const { createCanvas, registerFont } = require("canvas");
+const { createCanvas } = require("canvas");
 const express = require("express");
 const app = express();
-
-app.use("/fonts", express.static(__dirname + "/fonts"));
 
 // Function to convert color to valid CSS format (name or hex code)
 function getColorValue(color) {
@@ -60,13 +58,9 @@ app.get("/:dimensions", (req, res) => {
   ctx.fillStyle = canvasBackgroundColor;
   ctx.fillRect(0, 0, canvasWidth, canvasHeight);
 
-  // register font
-  const fontPath = __dirname + "/fonts/OpenSans-Bold.ttf";
-  registerFont(fontPath, { family: "Open Sans" });
-
   // Set text color and font
   ctx.fillStyle = canvasTextColor;
-  ctx.font = `${canvasFontSize}px "Open Sans"`;
+  ctx.font = `${canvasFontSize}px "Helvetica Neue", Helvetica, Arial, sans-serif`;
 
   // Measure text dimensions
   const textMetrics = ctx.measureText(canvasText);
